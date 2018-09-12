6bbfc00712a53fb21ccc67a596dafd39b0cc06e8
Manivas Murugaiah <manivas.m@gmail.com>
2018-09-12 15:42:14 -0500

    Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             //Code edited using github
             //Code added using VS
             //Code added to Feature 1
+            //Updating in master
         }
     }
 }
